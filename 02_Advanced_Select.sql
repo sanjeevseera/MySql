1783aee8ba09bcb7dec09fe2b38febfaad7e5536
sanjeevs <seerasanjeev@gmail.com>
2018-12-14 11:27:14 +0530

    02_Advanced_Select.sql

--- 02_Advanced_Select.sql
+++ 02_Advanced_Select.sql
@@ -40,3 +40,49 @@ select CONCAT(name,'(',SUBSTRING(occupation,1,1),')') from OCCUPATIONS order by 
 
 select "There are a total of",count(occupation),concat(lower(occupation),'s.') from OCCUPATIONS 
 group by occupation ORDER by count(occupation) ASC;
+
+/*
+You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+
+Column	Type
+---------------
+N	Integer
+P	Integer
+
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+Sample Input:
+
+N	P
+----------
+1	2
+3	2
+6	8
+9	8
+2	5
+8	5
+5	Null
+
+
+Sample Output:
+
+1 Leaf
+2 Inner
+3 Leaf
+5 Root
+6 Leaf
+8 Inner
+9 Leaf
+*/
+select
+    case
+        when P is NULL then CONCAT(N,' Root')
+        when N in (select P from BST) then CONCAT(N,' Inner')
+        else CONCAT(N,' Leaf')
+    end
+from BST order by N ASC;
+
+
