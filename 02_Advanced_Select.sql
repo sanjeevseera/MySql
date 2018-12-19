043624da29d4cc97ddf1edcb0401b1fee58362ab
sanjeevs <seerasanjeev@gmail.com>
2018-12-19 22:18:24 +0530

    02_Advanced_Select.sql

--- 02_Advanced_Select.sql
+++ 02_Advanced_Select.sql
@@ -7,9 +7,6 @@ Isosceles: It's a triangle with 2 sides of equal length.
 Scalene: It's a triangle with 3 sides of differing lengths.
 Not A Triangle: The given values of A, B, and C don't form a triangle.
 */
-/*
-Enter your query here.
-*/
 select
     case
         when A+B>C and A+C>B and B+C>A then
@@ -86,3 +83,41 @@ select
 from BST order by N ASC;
 
 
+/*
+Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+
+Note: Print NULL when there are no more names corresponding to an occupation.
+*/
+set @r1=0, @r2=0, @r3=0, @r4=0;
+select min(Doctor), min(Professor), min(Singer), min(Actor)
+from(
+  select case when Occupation='Doctor' then (@r1:=@r1+1)
+            when Occupation='Professor' then (@r2:=@r2+1)
+            when Occupation='Singer' then (@r3:=@r3+1)
+            when Occupation='Actor' then (@r4:=@r4+1) end as RowNumber,
+    case when Occupation='Doctor' then Name end as Doctor,
+    case when Occupation='Professor' then Name end as Professor,
+    case when Occupation='Singer' then Name end as Singer,
+    case when Occupation='Actor' then Name end as Actor
+  from OCCUPATIONS
+  order by Name
+) Temp
+group by RowNumber;
+
+/*
+Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees. Order your output by ascending company_code.
+
+Note:
+
+The tables may contain duplicate records.
+The company_code is string, so the sorting should not be numeric. For example, if the company_codes are C_1, C_2, and C_10, then the ascending company_codes will be C_1, C_10, and C_2.
+*/
+select E.company_code,C.founder, 
+count(distinct LM.lead_manager_code),count(distinct SM.senior_manager_code), 
+count(distinct M.manager_code),count(distinct E.employee_code) 
+from Company C 
+Join Lead_Manager LM on C.company_code=LM.company_code 
+Left Join Senior_Manager SM on LM.lead_manager_code=SM.lead_manager_code 
+Left Join Manager M on SM.senior_manager_code=M.senior_manager_code 
+Left Join Employee E on M.manager_code=E.manager_code 
+group by E.company_code,C.founder;
