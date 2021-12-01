dc81dd1651bbdffa8e229f627537be41fc43096f
Sanjeev Seera <seerasanjeev@gmail.com>
2021-12-01 17:39:19 +0530

    Update 01_Basic_Select.sql

--- 01_Basic_Select.sql
+++ 01_Basic_Select.sql
@@ -80,3 +80,10 @@ If there is more than one smallest or largest city, choose the one that comes fi
 */
 select min(city),min(length(city)) from station where (select MIN(LENGTH(CITY)) from station order BY length(city)) = length(city);
 select max(city),max(length(city)) from station where (select MAX(LENGTH(CITY)) from station order BY length(city)) = length(city);
+
+/*
+Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+*/
+select ROUND(sum(lat_n),2), ROUND(sum(long_w),2) from station;
