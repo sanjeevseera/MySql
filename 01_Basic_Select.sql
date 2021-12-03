93ab03b2d5fb43d74d79805397b75b3214696d39
Sanjeev Seera <seerasanjeev@gmail.com>
2021-12-03 10:40:43 +0530

    Update 01_Basic_Select.sql

--- 01_Basic_Select.sql
+++ 01_Basic_Select.sql
@@ -123,3 +123,8 @@ select ROUND(sum(salary)/count(salary)) -
  Then print these values as 2 space-separated integers.
  */
  select months*salary, count(months) from employee where (select MAX(months*salary) from employee order BY months*salary desc)=months*salary group by months*salary;
+
+/*
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places.
+*/
+select ROUND(long_w,4) from station where (select max(lat_n) from station where lat_n<137.2345)=lat_n;
